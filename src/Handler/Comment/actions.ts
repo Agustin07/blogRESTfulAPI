51d572567c8b9ea5ab8cc13f0c70f2f22b2f7ec8
Agus <rigoomartinez@gmail.com>
2020-05-29 11:33:01 -0600

    Update the readme information

--- src/Handler/Comment/actions.ts
+++ src/Handler/Comment/actions.ts
@@ -5,18 +5,18 @@ import {Comment} from "./schema";
 import mongoose from "mongoose";
 
 export const createComment = async (req : Request, res : Response) : Promise<Response> => {
-    const post = await Post.findById(req.params.id);
-    if(!post) return res.status(404).set("Content-Type", "text/plain").send("Invalid post id!");
-    if (post.isDeleted) return res.status(404).set("Content-Type", "text/plain").send("Post deleted!");
+	const post = await Post.findById(req.params.id);
+	if(!post) return res.status(404).set("Content-Type", "text/plain").send("Invalid post id!");
+	if (post.isDeleted) return res.status(404).set("Content-Type", "text/plain").send("Post deleted!");
 
-    const {error} = commentValidation.createValidation(req.body);
-    if (error) return res.status(400).set("Content-Type", "text/plain").send(error.message);
+	const {error} = commentValidation.createValidation(req.body);
+	if (error) return res.status(400).set("Content-Type", "text/plain").send(error.message);
     
-    const comment = new Comment(req.body); 
-    post.comments.push(comment);
-    try {
+	const comment = new Comment(req.body); 
+	post.comments.push(comment);
+	try {
 		const result = await post.save();
-		return res.status(200).set("Content-Type", "text/plain").send("Comment created!");
+		return res.status(201).location("/posts/"+result._id+"/comments").set("Content-Type", "text/plain").send("Created!");
 	}
 	catch (e) {
 		return res.status(404).set("Content-Type", "text/plain").send(e.message);
@@ -25,50 +25,50 @@ export const createComment = async (req : Request, res : Response) : Promise<Res
 
 
 export const getCommentById = async (req : Request, res : Response) : Promise<Response> => {
-    const post = await Post.findOne({ _id : req.params.id, isDeleted : false },{ comments: { $elemMatch : { _id: req.params.idc } } });
+	const post = await Post.findOne({ _id : req.params.id, isDeleted : false },{ comments: { $elemMatch : { _id: req.params.idc } } });
     
-    if( !post ) return res.status(404).set("Content-Type", "text/plain").send("Invalid post id!");
-    if( post.comments.length as number === 0 ) return res.status(404).set("Content-Type", "text/plain").send("Invalid comment id!");
-    if( post.comments[0].isDeleted ) return res.status(404).set("Content-Type", "text/plain").send("No found!");
-    return res.status(200).json(post.comments[0]);
+	if( !post ) return res.status(404).set("Content-Type", "text/plain").send("Invalid post id!");
+	if( post.comments.length as number === 0 ) return res.status(404).set("Content-Type", "text/plain").send("Invalid comment id!");
+	if( post.comments[0].isDeleted ) return res.status(404).set("Content-Type", "text/plain").send("No found!");
+	return res.status(200).json(post.comments[0]);
 };
 
 export const getCommentsByPostId = async (req : Request, res : Response) : Promise<Response> => {
-    const post = await Post.findById(req.params.id);
-    if(!post) return res.status(404).set("Content-Type", "text/plain").send("Invalid post id!");
-    if( post.comments.length as number === 0) return res.status(404).set("Content-Type", "text/plain").send("This post does not have any comment!");
-    const comments = post.comments.filter(cmmnt => (cmmnt.isDeleted === false ));
-    return res.status(200).json(comments);
+	const post = await Post.findById(req.params.id);
+	if(!post) return res.status(404).set("Content-Type", "text/plain").send("Invalid post id!");
+	if( post.comments.length as number === 0) return res.status(404).set("Content-Type", "text/plain").send("This post does not have any comment!");
+	const comments = post.comments.filter(cmmnt => (cmmnt.isDeleted === false ));
+	return res.status(200).json(comments);
 };
 
 export const deleteComment = async (req : Request, res : Response) : Promise<Response> => {
-    const id = mongoose.Types.ObjectId(req.params.id);
-    const idc = mongoose.Types.ObjectId(req.params.idc);
+	const id = mongoose.Types.ObjectId(req.params.id);
+	const idc = mongoose.Types.ObjectId(req.params.idc);
    
-    const post = await Post.findOneAndUpdate({ _id : id, isDeleted : false, comments : { $elemMatch : {_id : idc} }},
-        { $set: { "comments.$.isDeleted" : true } }, { new : true });
+	await Post.findOneAndUpdate({ _id : id, isDeleted : false, comments : { $elemMatch : {_id : idc} }},
+		{ $set: { "comments.$.isDeleted" : true } }, { new : true });
 
-    return res.status(200).set("Content-Type", "text/plain").send("Deleted!");
+	return res.status(200).set("Content-Type", "text/plain").send("Deleted!");
 };
 
 
 export const updateComment = async (req : Request, res : Response) : Promise<Response> => {
-    const id = mongoose.Types.ObjectId(req.params.id);
-    const idc = mongoose.Types.ObjectId(req.params.idc);
+	const id = mongoose.Types.ObjectId(req.params.id);
+	const idc = mongoose.Types.ObjectId(req.params.idc);
 
-    const {error} = commentValidation.updateValidation(req.body);
-    if (error) return res.status(400).set("Content-Type", "text/plain").send(error.message);
+	const {error} = commentValidation.updateValidation(req.body);
+	if (error) return res.status(400).set("Content-Type", "text/plain").send(error.message);
     
-    try{
-        const option = { new : true };
-        const post = await Post.findOneAndUpdate({ _id : id, isDeleted : false, comments : { $elemMatch : {_id : idc} }},
-            { $set: { "comments.$.content" : req.body.content } }, option);
-        if(!post) return res.status(404).set("Content-Type", "text/plain").send("Invalid id!");
-        return res.status(200).json(post);
-    }
-    catch(e){
-        return res.status(404).set("Content-Type", "text/plain").send(e.message);
-    }
+	try{
+		const option = { new : true };
+		const post = await Post.findOneAndUpdate({ _id : id, isDeleted : false, comments : { $elemMatch : {_id : idc} }},
+			{ $set: { "comments.$.content" : req.body.content } }, option);
+		if(!post) return res.status(404).set("Content-Type", "text/plain").send("Invalid id!");
+		return res.status(200).json(post);
+	}
+	catch(e){
+		return res.status(404).set("Content-Type", "text/plain").send(e.message);
+	}
 };
 
 
--- src/Handler/Tag/actions.ts
+++ src/Handler/Tag/actions.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from "express";
 import {Post} from "../Post/schema";
-import mongoose from "mongoose";
 
 import Joi from "joi";
 
 const createTagSchema = {
-    tag : Joi.string().min(1).max(150).required()
+	tag : Joi.string().min(1).max(150).required()
 };
 
 export const createValidation = (tag : string) : Joi.ValidationResult<string> => { 
@@ -13,15 +12,15 @@ export const createValidation = (tag : string) : Joi.ValidationResult<string> =>
 };
 
 export const createTag = async (req : Request, res : Response) : Promise<Response> => {
-    const post = await Post.findById(req.params.id);
-    if(!post) return res.status(404).set("Content-Type", "text/plain").send("Invalid post id!");
-    if (post.isDeleted) return res.status(404).set("Content-Type", "text/plain").send("Post deleted!");
+	const post = await Post.findById(req.params.id);
+	if(!post) return res.status(404).set("Content-Type", "text/plain").send("Invalid post id!");
+	if (post.isDeleted) return res.status(404).set("Content-Type", "text/plain").send("Post deleted!");
 
-    const {error} = createValidation(req.body);
-    if (error) return res.status(400).set("Content-Type", "text/plain").send(error.message);
+	const {error} = createValidation(req.body);
+	if (error) return res.status(400).set("Content-Type", "text/plain").send(error.message);
 
-    post.tags.push(req.body.tag);
-    try {
+	post.tags.push(req.body.tag);
+	try {
 		const result = await post.save();
 		return res.status(200).json(result);
 	}
@@ -31,18 +30,18 @@ export const createTag = async (req : Request, res : Response) : Promise<Respons
 };
 
 export const getAllTags = async (req : Request, res : Response) : Promise<Response> => {
-    const post = await Post.findOne({ _id : req.params.id, isDeleted : false }).select('tags');
-    if(!post) return res.status(404).set("Content-Type", "text/plain").send("Invalid id!");
+	const post = await Post.findOne({ _id : req.params.id, isDeleted : false }).select("tags");
+	if(!post) return res.status(404).set("Content-Type", "text/plain").send("Invalid id!");
 	if (post.isDeleted) return res.status(404).set("Content-Type", "text/plain").send("Post deleted!");
-    return res.status(200).json(post.tags);
+	return res.status(200).json(post.tags);
 };
 
 export const deleteTag = async (req : Request, res : Response) : Promise<Response> => {
-    const option = { new : true };
-    const post = await Post.findOneAndUpdate({ _id : req.params.id, isDeleted : false},
-        { $pull: { tags: req.params.name } }, option);
-    if(!post) return res.status(404).set("Content-Type", "text/plain").send("Invalid post id!");
-    return res.status(200).json(post);
+	const option = { new : true };
+	const post = await Post.findOneAndUpdate({ _id : req.params.id, isDeleted : false},
+		{ $pull: { tags: req.params.name } }, option);
+	if(!post) return res.status(404).set("Content-Type", "text/plain").send("Invalid post id!");
+	return res.status(200).json(post);
 };
 
 
